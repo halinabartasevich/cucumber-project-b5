6e0328e83512eaa03bb90e03deec9eb07b5636c8
halinabartasevich <hbartasevich@gmail.com>
2025-09-28 09:26:53 -0400

    day14

--- src/test/java/io/loop/utilities/Driver.java
+++ src/test/java/io/loop/utilities/Driver.java
@@ -55,6 +55,12 @@ public class Driver {
                 }
                 case "firefox" -> driverPool.set(new FirefoxDriver());
                 case "safari" -> driverPool.set(new SafariDriver());
+                case "headless" -> {
+                    options.addArguments("--disable-blink-features=AutomationControlled");
+                    options.addArguments("--headless");
+                    driverPool.set(new ChromeDriver(options));
+
+                }
             }
             driverPool.get().manage().window().maximize();
             driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
---
+++ src/test/java/io/loop/utilities/TestExcel.java
@@ -0,0 +1,19 @@
+package io.loop.utilities;
+
+public class TestExcel {
+
+    public static void main(String[] args) {
+
+
+        ExcelUtils excelUtils = new ExcelUtils("/!!!!!!!!!I NEED TO ADD HERE....", "Sheet1");
+
+        System.out.println("excelUtils.getCellData(0,0) = " + excelUtils.getCellData(0, 0));
+        System.out.println("excelUtils.getCellData(0,0) = " + excelUtils.getCellData(0, 1));
+
+        excelUtils.setCellData("Owner", 0, 2);
+        excelUtils.setCellData("Feyruz", 1, 2);
+
+
+    }
+
+}
